499dba430f3a4d530db1c5fbb054e74718a4f77a
PavelZubkov <PavelZubkov@users.noreply.github.com>
2023-10-06 07:15:18 +0000

    Deploying to gh-pages from @ PavelZubkov/visavis@b9e61e4942a39112d5c225f4bef14134ba8a1cb5 🚀

--- node.test.js
+++ node.test.js
@@ -7770,6 +7770,9 @@ var $;
                 ]);
                 el.selectionEnd = to;
                 el.selectionStart = from;
+                if (to !== from && el.selectionEnd === el.selectionStart) {
+                    el.selectionEnd = to;
+                }
             }
             selection_start() {
                 const el = this.dom_node();

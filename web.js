40effc033936d9b75b093e0a6bedcccb395b28da
PavelZubkov <PavelZubkov@users.noreply.github.com>
2023-12-13 07:15:45 +0000

    Deploying to gh-pages from @ PavelZubkov/visavis@b9e61e4942a39112d5c225f4bef14134ba8a1cb5 🚀

--- web.js
+++ web.js
@@ -5435,8 +5435,8 @@ var $;
             return [];
         }
         Body_content() {
-            const obj = new this.$.$mol_list();
-            obj.rows = () => this.body();
+            const obj = new this.$.$mol_view();
+            obj.sub = () => this.body();
             return obj;
         }
         body_content() {
@@ -5561,6 +5561,9 @@ var $;
             },
             Body_content: {
                 padding: $mol_gap.block,
+                flex: {
+                    direction: 'column',
+                },
                 justify: {
                     self: 'stretch',
                 },

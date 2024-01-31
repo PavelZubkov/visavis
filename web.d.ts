dc6f430794ea07365b12e5dd4e258ff8c31630d1
PavelZubkov <PavelZubkov@users.noreply.github.com>
2024-01-31 07:15:20 +0000

    Deploying to gh-pages from @ PavelZubkov/visavis@b9e61e4942a39112d5c225f4bef14134ba8a1cb5 🚀

--- web.d.ts
+++ web.d.ts
@@ -1782,15 +1782,8 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    function $mol_diff_path<Item>(...paths: Item[][]): {
-        prefix: Item[];
-        suffix: Item[][];
-    };
-}
-
-declare namespace $ {
-    class $mol_error_mix extends Error {
-        errors: Error[];
+    class $mol_error_mix extends AggregateError {
+        name: string;
         constructor(message: string, ...errors: Error[]);
         toJSON(): string;
     }
@@ -1798,6 +1791,7 @@ declare namespace $ {
 
 declare namespace $ {
     class $mol_data_error extends $mol_error_mix {
+        name: string;
     }
 }
 
